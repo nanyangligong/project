6c50d314d79688d366320c22d153eac0eee7dfb1
nanyangligong <3287616935@qq.com>
2018-09-17 09:59:01 +0800

    config

---
+++ secondhandbooksell/src/main/java/cn/edu/nyist/secondhandbooksell/SecondhandbooksellApplication.java
@@ -0,0 +1,12 @@
+package cn.edu.nyist.secondhandbooksell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondhandbooksellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondhandbooksellApplication.class, args);
+	}
+}
---
+++ secondhandbooksell/src/test/java/cn/edu/nyist/secondhandbooksell/SecondhandbooksellApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.edu.nyist.secondhandbooksell;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SecondhandbooksellApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
